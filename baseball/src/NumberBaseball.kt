797a502ba204f6b77c67d8301705f9b30e32b83c
jeseong <mywptjd0127@naver.com>
2022-05-26 17:09:48 +0900

    Feat: 숫자야구게임 구현

---
+++ baseball/src/NumberBaseball.kt
@@ -0,0 +1,59 @@
+import java.util.*
+
+val scanner = Scanner(System.`in`)
+
+fun main() {
+    while (true) {
+        // 자동번호 받기
+        val generatedNumber = generateRandomNumbers()
+
+        while (true) {
+            // 사용자 입력 받기
+            val numbers = getNumbersFromUser()
+
+            // 비교하기 && 결과값에 따라 반복유무 체크
+            if (compareNumbers(generatedNumber, numbers))
+                break
+        }
+
+        // 게임진행 여부 체크
+        if (!isCoinInserted())
+            break
+    }
+}
+
+fun generateRandomNumbers(): IntArray {
+    val numbers = IntArray(3)
+    for (i in 0..2) {
+        numbers[i] = (1..9).random()
+        print(numbers[i])
+    }
+    println()
+    return numbers
+}
+
+fun getNumbersFromUser(): String {
+    print("숫자를 입력해주세요: ")
+    return scanner.next()
+}
+
+fun compareNumbers(auto: IntArray, user: String): Boolean {
+    var strike = 0
+    var ball = 0
+
+    for (i in 0..2) {
+        if (user.indexOf("" + auto[i], i) == i)
+            strike++
+        else if (user.contains("" + auto[i]))
+            ball++
+    }
+    println("strike: $strike ball: $ball")
+
+    return strike == 3
+}
+
+fun isCoinInserted(): Boolean {
+    println("숫자를 모두 맞히셨습니다! 게임종료")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요")
+    return scanner.nextInt() == 1
+}
